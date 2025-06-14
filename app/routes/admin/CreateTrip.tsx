12470b4c1f84dc6ca1ee1c5dacf0c85fdd7be412
faisal-din <dahrifaisal56@gmail.com>
2025-06-14 17:49:12 +0500

    generate trip using gemini api and unsplash api

--- app/routes/admin/CreateTrip.tsx
+++ app/routes/admin/CreateTrip.tsx
@@ -13,6 +13,7 @@ import { world_map } from '~/constants/world_map';
 import { useState } from 'react';
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
 import { account } from '~/appwrite/client';
+import { useNavigate } from 'react-router';
 
 export const loader = async () => {
   //   const response = await fetch('https://restcountries.com/v3.1/all');
@@ -33,6 +34,8 @@ export const loader = async () => {
 
 const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
   const countries = loaderData as Country[];
+
+  const navigate = useNavigate();
 
   const [formData, setFormData] = useState<TripFormData>({
     country: countries[0]?.name || '',
@@ -74,6 +77,31 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
       console.error('User not authenticated');
       setLoading(false);
       return;
+    }
+
+    try {
+      const response = await fetch('/api/create-trip', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          country: formData.country,
+          numberOfDays: formData.duration,
+          travelStyle: formData.travelStyle,
+          interests: formData.interest,
+          budget: formData.budget,
+          groupType: formData.groupType,
+          userId: user.$id,
+        }),
+      });
+
+      const result: CreateTripResponse = await response.json();
+
+      if (result?.id) navigate(`/trips/${result.id}`);
+      else console.error('Failed to generate a trip');
+    } catch (error) {
+      console.error('Error generating trip:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
