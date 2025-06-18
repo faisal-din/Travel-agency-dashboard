c1758d0503288f19465339dff81d98193bf1fb38
faisal-din <dahrifaisal56@gmail.com>
2025-06-18 20:08:12 +0500

    stripe payment functionality added

--- app/routes/API/create-trip.ts
+++ app/routes/API/create-trip.ts
@@ -1,8 +1,9 @@
+import { type ActionFunctionArgs, data } from 'react-router';
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import { ID } from 'appwrite';
-import { data, type ActionFunctionArgs } from 'react-router';
+import { parseMarkdownToJson, parseTripData } from '~/lib/utils';
 import { appwriteConfig, database } from '~/appwrite/client';
-import { parseMarkdownToJson } from '~/lib/utils';
+import { ID } from 'appwrite';
+import { createProduct } from '~/lib/stripe';
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const {
@@ -20,50 +21,50 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
   try {
     const prompt = `Generate a ${numberOfDays}-day travel itinerary for ${country} based on the following user information:
-    Budget: '${budget}'
-    Interests: '${interests}'
-    TravelStyle: '${travelStyle}'
-    GroupType: '${groupType}'
-    Return the itinerary and lowest estimated price in a clean, non-markdown JSON format with the following structure:
-    {
-    "name": "A descriptive title for the trip",
-    "description": "A brief description of the trip and its highlights not exceeding 100 words",
-    "estimatedPrice": "Lowest average price for the trip in USD, e.g.$price",
-    "duration": ${numberOfDays},
-    "budget": "${budget}",
-    "travelStyle": "${travelStyle}",
-    "country": "${country}",
-    "interests": ${interests},
-    "groupType": "${groupType}",
-    "bestTimeToVisit": [
-      '🌸 Season (from month to month): reason to visit',
-      '☀️ Season (from month to month): reason to visit',
-      '🍁 Season (from month to month): reason to visit',
-      '❄️ Season (from month to month): reason to visit'
-    ],
-    "weatherInfo": [
-      '☀️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
-      '🌦️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
-      '🌧️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
-      '❄️ Season: temperature range in Celsius (temperature range in Fahrenheit)'
-    ],
-    "location": {
-      "city": "name of the city or region",
-      "coordinates": [latitude, longitude],
-      "openStreetMap": "link to open street map"
-    },
-    "itinerary": [
-    {
-      "day": 1,
-      "location": "City/Region Name",
-      "activities": [
-        {"time": "Morning", "description": "🏰 Visit the local historic castle and enjoy a scenic walk"},
-        {"time": "Afternoon", "description": "🖼️ Explore a famous art museum with a guided tour"},
-        {"time": "Evening", "description": "🍷 Dine at a rooftop restaurant with local wine"}
-      ]
-    },
-    ...
-    ]
+        Budget: '${budget}'
+        Interests: '${interests}'
+        TravelStyle: '${travelStyle}'
+        GroupType: '${groupType}'
+        Return the itinerary and lowest estimated price in a clean, non-markdown JSON format with the following structure:
+        {
+        "name": "A descriptive title for the trip",
+        "description": "A brief description of the trip and its highlights not exceeding 100 words",
+        "estimatedPrice": "Lowest average price for the trip in USD, e.g.$price",
+        "duration": ${numberOfDays},
+        "budget": "${budget}",
+        "travelStyle": "${travelStyle}",
+        "country": "${country}",
+        "interests": ${interests},
+        "groupType": "${groupType}",
+        "bestTimeToVisit": [
+          '🌸 Season (from month to month): reason to visit',
+          '☀️ Season (from month to month): reason to visit',
+          '🍁 Season (from month to month): reason to visit',
+          '❄️ Season (from month to month): reason to visit'
+        ],
+        "weatherInfo": [
+          '☀️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
+          '🌦️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
+          '🌧️ Season: temperature range in Celsius (temperature range in Fahrenheit)',
+          '❄️ Season: temperature range in Celsius (temperature range in Fahrenheit)'
+        ],
+        "location": {
+          "city": "name of the city or region",
+          "coordinates": [latitude, longitude],
+          "openStreetMap": "link to open street map"
+        },
+        "itinerary": [
+        {
+          "day": 1,
+          "location": "City/Region Name",
+          "activities": [
+            {"time": "Morning", "description": "🏰 Visit the local historic castle and enjoy a scenic walk"},
+            {"time": "Afternoon", "description": "🖼️ Explore a famous art museum with a guided tour"},
+            {"time": "Evening", "description": "🍷 Dine at a rooftop restaurant with local wine"}
+          ]
+        },
+        ...
+        ]
     }`;
 
     const textResult = await genAI
@@ -92,9 +93,27 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       }
     );
 
+    const tripDetail = parseTripData(result.tripDetails) as Trip;
+    const tripPrice = parseInt(tripDetail.estimatedPrice.replace('$', ''), 10);
+    const paymentLink = await createProduct(
+      tripDetail.name,
+      tripDetail.description,
+      imageUrls,
+      tripPrice,
+      result.$id
+    );
+
+    await database.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.tripCollectionId,
+      result.$id,
+      {
+        payment_link: paymentLink.url,
+      }
+    );
+
     return data({ id: result.$id });
-  } catch (error) {
-    console.error('Error generating trip:', error);
-    return { error: 'Failed to generate trip' };
+  } catch (e) {
+    console.error('Error generating travel plan: ', e);
   }
 };
